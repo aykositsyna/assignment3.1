3e0ee2e1a310144a6093090632c2f7d3aaf1a81d
Alina <a.y.kositsyna@gmail.com>
2023-10-10 22:03:23 +0700

    created class BussinessLogic. Linked Model. Created List of students. Created methids: AddStudent, RemoveStudent, GetAllStudents, GetHistogram. Added StudentCount

--- BusinessLogic/Logic.cs
+++ BusinessLogic/Logic.cs
@@ -13,19 +13,40 @@ namespace BusinessLogic
     public class Logic
     {
 
-        private List<Student> Students { get; set; } = new List<Student>();
+        public int StudentCount => Students.Count;
 
+        private List<Student> Students = new List<Student>()
+        {
+            new Student() {Name = "Ivan", Speciality = "History", Group = "AB22-02"},
+            new Student() {Name = "Egor", Speciality = "SKD", Group = "AB10-01"}
+        };
+
+
+        /// <summary>
+        /// adding new student
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="speciality"></param>
+        /// <param name="group"></param>
         public void AddStudent(string name, string speciality, string group)
         {
-            Student student = new Student();
+            Student student = new Student() { Name = name, Speciality = speciality, Group = group};
             Students.Add(student);
         }
 
+        /// <summary>
+        /// removing student
+        /// </summary>
+        /// <param name="index"></param>
         public void RemoveStudent(int index)
         {
             Students.RemoveAt(index);
         }
 
+        /// <summary>
+        /// get all students
+        /// </summary>
+        /// <returns></returns>
         public List<string> GetAllStudents()
         {
             List<string> result = new List<string>();
@@ -36,5 +57,29 @@ namespace BusinessLogic
             }
             return result;
         }
+
+
+        /// <summary>
+        /// creating dictionary for histogram
+        /// </summary>
+        /// <returns></returns>
+        public Dictionary<string, int> GetHistogram()
+        {
+            Dictionary<string, int> res = new Dictionary<string, int>();
+            foreach (Student student in Students)
+            {
+                if (res.ContainsKey(student.Speciality))
+                {
+                    res[student.Speciality]++;
+                }
+                else
+                {
+                    res.Add(student.Speciality, 1);
+                }
+            }
+            return res;
+        }
+
+    
     }
 }
