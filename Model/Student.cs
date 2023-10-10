456ff0a9e660f304b17ebcde1295489ccea7a52a
Alina <a.y.kositsyna@gmail.com>
2023-10-10 22:04:20 +0700

    Created project Model with class Student

--- Model/Student.cs
+++ Model/Student.cs
@@ -11,8 +11,19 @@ namespace Model
     /// </summary>
     public class Student
     {
+        /// <summary>
+        /// student's name
+        /// </summary>
         public string Name { get; set; }
+
+        /// <summary>
+        /// student's speciality
+        /// </summary>
         public string Speciality { get; set; }
+
+        /// <summary>
+        /// student's group
+        /// </summary>
         public string Group { get; set; }
     }
 }
