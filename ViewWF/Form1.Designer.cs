eb7355d9b0bfecb391f5a054a99d73ff23d997f9
Alina <a.y.kositsyna@gmail.com>
2023-10-10 22:12:19 +0700

    Created project ViewWF. Created WF constructor. created events for button clicks.

--- ViewWF/Form1.Designer.cs
+++ ViewWF/Form1.Designer.cs
@@ -28,13 +28,88 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.AddStudentBttn = new System.Windows.Forms.Button();
+            this.RemoveStudentBttn = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.StudentsListView = new System.Windows.Forms.ListView();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // AddStudentBttn
+            // 
+            this.AddStudentBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.AddStudentBttn.ForeColor = System.Drawing.Color.ForestGreen;
+            this.AddStudentBttn.Location = new System.Drawing.Point(12, 387);
+            this.AddStudentBttn.Name = "AddStudentBttn";
+            this.AddStudentBttn.Size = new System.Drawing.Size(189, 51);
+            this.AddStudentBttn.TabIndex = 1;
+            this.AddStudentBttn.Text = "Add student";
+            this.AddStudentBttn.UseVisualStyleBackColor = true;
+            this.AddStudentBttn.Click += new System.EventHandler(this.AddStudentBttn_Click);
+            // 
+            // RemoveStudentBttn
+            // 
+            this.RemoveStudentBttn.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.RemoveStudentBttn.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.RemoveStudentBttn.Location = new System.Drawing.Point(233, 387);
+            this.RemoveStudentBttn.Name = "RemoveStudentBttn";
+            this.RemoveStudentBttn.Size = new System.Drawing.Size(189, 51);
+            this.RemoveStudentBttn.TabIndex = 2;
+            this.RemoveStudentBttn.Text = "Remove student";
+            this.RemoveStudentBttn.UseVisualStyleBackColor = true;
+            this.RemoveStudentBttn.Click += new System.EventHandler(this.RemoveStudentBttn_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(426, 30);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(362, 308);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // StudentsListView
+            // 
+            this.StudentsListView.HideSelection = false;
+            this.StudentsListView.Location = new System.Drawing.Point(12, 30);
+            this.StudentsListView.Name = "StudentsListView";
+            this.StudentsListView.Size = new System.Drawing.Size(382, 308);
+            this.StudentsListView.TabIndex = 4;
+            this.StudentsListView.UseCompatibleStateImageBehavior = false;
+            this.StudentsListView.SelectedIndexChanged += new System.EventHandler(this.StudentsListView_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.StudentsListView);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.RemoveStudentBttn);
+            this.Controls.Add(this.AddStudentBttn);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+        private System.Windows.Forms.Button AddStudentBttn;
+        private System.Windows.Forms.Button RemoveStudentBttn;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.ListView StudentsListView;
     }
 }
 
--- ViewWF/Form1.cs
+++ ViewWF/Form1.cs
@@ -7,14 +7,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BusinessLogic;
 
 namespace ViewWF
 {
     public partial class Form1 : Form
     {
+        
         public Form1()
         {
+            Logic Logic = new Logic();
             InitializeComponent();
+
+            StudentsListView.Columns.Add("Students");
+            StudentsListView.Columns.Add("Speciality");
+            StudentsListView.Columns.Add("Group");
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void AddStudentBttn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RemoveStudentBttn_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void StudentsListView_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Refresh()
+        {
+
         }
     }
 }
