751d649ca2de465d83805a737750ad8b6f9d3130
Alina <a.y.kositsyna@gmail.com>
2023-10-24 22:33:30 +0700

    (fix)[]: some set up shit

---
+++ DataAccessLayer/Context.cs
@@ -0,0 +1,39 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Npgsql;
+
+namespace DataAccessLayer
+{
+    public class Context : DbContext
+    {
+        public Context() : base("DbConnection") { }
+
+        public DbSet<Student> Students { get; set; }
+    }
+
+    //public class EntityRepository<T> : IRepository<T> where T : class, IDomainObject, new()
+    //{
+    //    public Context _context;
+
+    //    public EntityRepository(Context context)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    public IEnumerable<T> GetAll()
+    //    {
+    //        return _context.Set<T>();
+    //    }
+
+    //    public void Create(T obj)
+    //    {
+    //        _context.Set<T>().Add(obj);
+    //    }
+
+    //}
+}
---
+++ DataAccessLayer/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    internal interface IRepository<T> : IDisposable where T : class
+    {
+        IEnumerable<T> GetStudentsList();
+        T GetStudent(int id);
+        void Create(T student);
+        void Update(T student);
+        void Delete(T student);
+    }
+}
