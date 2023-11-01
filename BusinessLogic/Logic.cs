3c9ccb9fe49697734048a3992f0308f024f20939
Alina <a.y.kositsyna@gmail.com>
2023-11-01 14:47:35 +0700

    (fix)[Logic]: changed methods from List<Student> to repository

--- BusinessLogic/Logic.cs
+++ BusinessLogic/Logic.cs
@@ -13,27 +13,27 @@ namespace BusinessLogic
     /// </summary>
     public class Logic
     {
-        //public EntityRepository<Student> repository = new EntityRepository<Student>();
+        public EntityFrameworkRepository<Student> repository = new EntityFrameworkRepository<Student>(new Context());
 
         public int StudentCount => Students.Count;
 
-        private List<Student> Students = new List<Student>();
-        //{
-        //    new Student() {Name = "Ivan", Speciality = "History", Group = "AB22-02"},
-        //    new Student() {Name = "Egor", Speciality = "SKD", Group = "AB10-01"}
-        //};
+        private List<Student> Students = new List<Student>()
+        {
+            new Student() { Name = "Ivan", Speciality = "History", Group = "AB22-02", ID = 111},
+            new Student() { Name = "Egor", Speciality = "SKD", Group = "AB10-01", ID = 112}
+        };
 
 
-        /// <summary>
-        /// adding new student
-        /// </summary>
-        /// <param name="name"></param>
-        /// <param name="speciality"></param>
-        /// <param name="group"></param>
-        public void AddStudent(string name, string speciality, string group)
+    /// <summary>
+    /// adding new student
+    /// </summary>
+    /// <param name="name"></param>
+    /// <param name="speciality"></param>
+    /// <param name="group"></param>
+    public void AddStudent(string name, string speciality, string group)
         {
             Student student = new Student() { Name = name, Speciality = speciality, Group = group};
-            Students.Add(student);
+            repository.Create(student);
         }
 
         /// <summary>
@@ -42,7 +42,7 @@ namespace BusinessLogic
         /// <param name="index"></param>
         public void RemoveStudent(int index)
         {
-            Students.RemoveAt(index);
+            repository.Delete(index);
         }
 
         /// <summary>
@@ -52,10 +52,14 @@ namespace BusinessLogic
         public List<string> GetAllStudents()
         {
             List<string> result = new List<string>();
-            foreach (Student student in Students)
+            foreach (Student student in repository.GetStudentsList())
             {
-                string str = student.Name + "|" + student.Speciality + "|" + student.Group;
-                result.Add(str);
+                if (!string.IsNullOrEmpty(student.Name) && !string.IsNullOrEmpty(student.Speciality) && !string.IsNullOrEmpty(student.Group))
+                {
+                    string str = student.Name + "|" + student.Speciality + "|" + student.Group + "|" + student.ID;
+                    result.Add(str);
+                }
+
             }
             return result;
         }
