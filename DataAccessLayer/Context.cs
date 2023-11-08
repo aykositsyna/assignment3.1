374b0a7bee34b0ca22960d9434e70fe0be897f7b
Alina <a.y.kositsyna@gmail.com>
2023-11-08 15:16:19 +0700

    (fix) connected LocalDB through context

--- DataAccessLayer/Context.cs
+++ DataAccessLayer/Context.cs
@@ -11,7 +11,7 @@ namespace DataAccessLayer
 {
     public class Context : DbContext
     {
-        public Context() : base("DbConnection") { }
+        public Context() : base("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Станислав\\Documents\\progrmming3\\1\\assignment3.1\\DataAccessLayer\\LocalDB.mdf;Integrated Security=True") { }
 
         public DbSet<Student> Students { get; set; }
     }
